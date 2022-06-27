19ecd2e1e227d7d14d766a9c412f820b36ad0136
LeonardoBujan <tatobujan86@gmail.com>
2022-06-27 03:05:52 -0300

    instala paquete metrod override, agrega formato json para capturar valores de body y crea página de error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "./public");
+const methodOverride = require("method-override");
 
 //Require de rutas
 const routesMain = require("./src/routes/main.js");
@@ -15,8 +16,15 @@ app.listen(3000, ()=>{
     console.log("Servidor corriendo en el puerto 3000");
 });
 
+//Indicar a Express que el formato del body sea json para capturar los valores
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
 //Compartir la carpeta public con las vistas
 app.use(express.static(publicPath));
+
+//Indicar al proyecto que use method-override para incorporar PUT / DELETE
+app.use(methodOverride("_method"));
 
 //Template Engine
 app.set("view engine", "ejs");
@@ -27,3 +35,8 @@ app.use("/menu/ventas", routesBills);
 app.use("/menu/pacientes", routesPatients);
 app.use("/menu/productos", routesProducts);
 app.use("/menu/turnos", routesTurns);
+
+//Enviar error cuando la url no existe
+app.use((req, res, next) => {
+    res.status(404).render("not-found");
+});
